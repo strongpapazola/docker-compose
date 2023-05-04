8ed840537b89718d966ab15d8181b6e4755d003c
strongpapazola <48545591+strongpapazola@users.noreply.github.com>
2023-05-04 23:46:40 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+#v2.17.3
+DESTINATION=/usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+sudo chmod 755 $DESTINATION
+echo "[*] docker-compose $VERSION installed !"
